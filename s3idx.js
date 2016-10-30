a90c1a10fd625aa2a7d83adabd82bc22f4584f93
Tamaki Ichino <tmkicn@gmail.com>
2016-10-30 21:37:08 +0900

    update s3idx.js

--- s3idx.js
+++ s3idx.js
@@ -1,54 +1,50 @@
-'use strict';
+/*
+** s3idx.js
+** generates file index of AWS S3 Bucket dynamically
+** 2016.10.26 ichino
+*/
 
-console.log('Loading function');
+'use strict';
 
 var aws = require('aws-sdk');
 var s3 = new aws.S3({ apiVersion: '2006-03-01' });
+var myBucket = "to.dist.bioweather.net";
 var idxFile = "files.txt";
 
 exports.handler = function(event, context){
-    var bucket = event.Records[0].s3.bucket.name;
-    var key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
-    var params = {
-        Bucket: bucket,
-        Key: key,
-    };
-    s3.getObject(params, function(err, data){
-        var msg;
-        if (err) {
-            console.log(err);
-            msg = "Error getting object " + key + " from bucket " + bucket + ".";
-            console.log(msg);
-            //context.fail(msg);
-        } else {
-            if (key != idxFile){
-                s3.listObjects({Bucket: 'test-icn'}, function(err, data){
-                    if(err){
-                        msg = "Error listing objects ... " ;
-                        console.log(msg);
-                    }else{
-                        var arr = [];
-                        for(var obj of data.Contents){
-                            arr.push(obj.Key);
-                        }
-                        var str = arr.join("\n");
-                        //console.log(str);
-                        var param = {
-                            Bucket: "test-icn",
-                            Key: idxFile,
-                            Body: str,
-                            ContentType: "text/plain",
-                        };
-                        s3.putObject(param, function(err){
-                            if(err){
-                                console.log(err);
-                            }else{
-                                console.log(idxFile + " successfully written");
-                            }
-                        });
+    //var key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
+    //if (key != idxFile){
+        var param = {
+          Bucket: myBucket,
+          MaxKeys: 1000,
+        };
+        var d = new Date();
+        var str = d.toString() + "\n\n";
+        s3.listObjects(param).eachPage(function(err, data){
+            if(err){
+                console.log(err);
+            }else{
+                if(data){
+                    for(var obj of data.Contents){
+                        str += obj.Key + "\n";
                     }
-                });
+                }else{
+                    //console.log(str);
+                    var param = {
+                        Bucket: myBucket,
+                        Key: idxFile,
+                        Body: str,
+                        ContentType: "text/plain",
+                    };
+                    s3.putObject(param, function(err){
+                        if(err){
+                            console.log(err);
+                        }else{
+                            console.log(idxFile + " successfully written");
+                        }
+                    });
+                }
             }
-        }
-    });
+        });
+//}
 };
